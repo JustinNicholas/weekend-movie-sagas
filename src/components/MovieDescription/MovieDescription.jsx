5afe284d6f145cdaa7d3f52330c48e3f377368f5
Justin Nicholas <nicholasj964@yahoo.com>
2022-08-26 23:39:03 -0500

    home button working.

--- src/components/MovieDescription/MovieDescription.jsx
+++ src/components/MovieDescription/MovieDescription.jsx
@@ -1,19 +1,32 @@
 import { useSelector } from "react-redux";
+import { useHistory } from "react-router-dom";
 
 function MovieDescription() {
 
+    const history = useHistory();
+
+    const returnHome = () => {
+        history.push('/');
+    }
+
     const description = useSelector(store => store.description)
     console.log('description:',description);
+
     return(
         <>
         {description.map( movie => {
             console.log('title:',movie.title);
             return (
-            <div key={movie.id}>
-                <h1>Description Page</h1>
-                <img src={movie.poster} />
-                <h3>{movie.title}</h3>
-                <p>{movie.description}</p>
+            <div>
+                <div key={movie.id}>
+                    <h1>Description Page</h1>
+                    <img src={movie.poster} />
+                    <h3>{movie.title}</h3>
+                    <p>{movie.description}</p>
+                </div>
+                <div>
+                    <button onClick={returnHome}>Home</button>
+                </div>
             </div>
             )
         })}
